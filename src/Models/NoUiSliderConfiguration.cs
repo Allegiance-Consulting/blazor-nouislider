17570438960935324e9a42309a8fb89beee48267
Johan <johan.dekock@allegiance.co.za>
2020-07-17 11:57:29 +0200

    Dual slide capibility

--- src/Models/NoUiSliderConfiguration.cs
+++ src/Models/NoUiSliderConfiguration.cs
@@ -7,14 +7,15 @@ namespace Allegiance.Blazor.NoUiSlider.Models
     public class NoUiSliderConfiguration<T>
     {
         public T Start { get; set; }
-        public bool Connect { get; set; } = true;
+        public object Connect { get; set; }
         public Range<T> Range { get; set; }
         public Guid Id { get; set; } = Guid.NewGuid();
+        public bool Tooltips { get; set; } = true;
+        public bool SetSlider { get; set; } = false;
     }
-
     public class Range<T>
     {
         public T Min { get; set; }
         public T Max { get; set; }
-    }
+    }    
 }
--- src/wwwroot/scripts/abnouislider.js
+++ src/wwwroot/scripts/abnouislider.js
@@ -1,7 +1,7 @@
 ﻿window.sliders = [];
 
 window.renderSlider = function (configuration, dotNetObjectReference) {
-    let slider = document.getElementById('slider');
+    let slider = document.getElementById(configuration.id);
     noUiSlider.create(slider, configuration);
     slider.noUiSlider.on("slide", function (val)
     {
@@ -13,9 +13,26 @@ window.renderSlider = function (configuration, dotNetObjectReference) {
 window.updateSlider = function (configuration) {
     window.sliders.forEach((value, index) => {
         if (value.configuration.id === configuration.id) {
-            configuration.start = value.slider.noUiSlider.get();
+            if (!configuration.setSlider) {
+                configuration.start = value.slider.noUiSlider.get();
+            } 
             value.slider.noUiSlider.updateOptions(configuration);
             value.configuration = configuration;
         }
     });
 }
+
+window.mask = (id) => {
+    var customMask = IMask(
+        document.getElementById(id), {
+        mask: "R num",
+        blocks: {
+            num: {
+                mask: Number,
+                thousandsSeparator: ' '
+            }
+        }
+
+    }
+    );
+};
