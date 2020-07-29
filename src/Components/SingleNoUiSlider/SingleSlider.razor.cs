3de563bd281f1e46907f58937c79aa7b7ea4fcae
Johan <johan.dekock@allegiance.co.za>
2020-07-29 11:02:29 +0200

    Added some secondary & tertiary slide colors

--- src/Components/SingleNoUiSlider/SingleSlider.razor.cs
+++ src/Components/SingleNoUiSlider/SingleSlider.razor.cs
@@ -1,10 +1,5 @@
 ﻿using Allegiance.Blazor.NoUiSlider.Models;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Net.NetworkInformation;
-using System.Runtime.CompilerServices;
-using System.Text;
+
 
 namespace Allegiance.Blazor.NoUiSlider.Components.SingleNoUiSlider
 {
@@ -15,31 +10,28 @@ namespace Allegiance.Blazor.NoUiSlider.Components.SingleNoUiSlider
 
         public NoUiSliderConfiguration<double> Config = new NoUiSliderConfiguration<double>
         {
-            //Start = 10,
             Range = new Range<double>
             {
                 Min = 0,
                 Max = 100
             },
             //To have the step linked to a percentage of max value
-            PercentageStep = 20,
-            //TooltipsFormat = new Models.TooltipsFormat
-            //{
-            //    Prefix = "",
-            //    Suffix = "%",
-            //    Decimals = 0
-            //}
+            PercentageStep = 15,
+            TooltipsFormat = new Models.TooltipsFormat
+            {
+                Prefix = "",
+                Suffix = "%",
+                Decimals = 0
+            }
         };
 
         public NoUiSliderConfiguration<double> Config2 = new NoUiSliderConfiguration<double>
         {
-            //Start = 30,
             Range = new Range<double>
             {
                 Min = 0,
                 Max = 100
             },
-            PercentageStep = 0,
         };
         public void Change()
         {
